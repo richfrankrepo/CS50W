514afe3d78943e7213277c00b2786247031dc68d
Richard Franklin <Rich.Frank@ntlworld.com>
2020-07-20 12:33:46 +0100

    Add Dictionaries sample

---
+++ Lecture 2 - Python/Dictionaries.py
@@ -0,0 +1,12 @@
+# Dictionaries in Python are unordered, changeable and indexed
+
+#Define the dictionary
+Houses = {"Harry": "Gryffindor","Draco":"Slytherin"}
+
+#look up and display the value of item 'Harry'
+print(Houses["Harry"])
+
+
+#Add another item to the dictionary, then display the value of item
+Houses["Hermione"] = "Gryffindor"
+print(Houses["Hermione"])
